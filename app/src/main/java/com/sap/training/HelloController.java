304959f7bc0f31bfd4d34cf2f2d977543651308c
Anju Kumawat <anjukumawat2004@gmail.com>
2026-08-28 11:31:54 +0530

    making changes

--- app/src/main/java/com/sap/training/HelloController.java
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Nandini This is a devops demo";
+        return "Hello from Anjuu , This is a devops demo";
     }
 
     @GetMapping("/health")
